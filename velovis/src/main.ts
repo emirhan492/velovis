b79162b1e6980dc1db09c4321d8b91268ee60e94
emirhan492 <ertergaming088@gmail.com>
2025-12-18 15:40:31 +0300

    velovis

--- velovis/src/main.ts
+++ velovis/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [ 
+      'http://localhost:3001',
+      'velovis-frontend.vercel.app',
+      'https://www.veloviswear.com',
+      'https://velovis.com',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
